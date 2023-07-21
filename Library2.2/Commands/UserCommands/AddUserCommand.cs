8516297600dcc9e05eaa7914511529b2c8df1c7e
VldislavKh <vkh123w@gmail.com>
2023-07-21 14:43:18 +0300

    добавлен контроллер UserController и команда AddUserCommand

---
+++ Library2.2/Commands/UserCommands/AddUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using Library2._2.Interfaces.UserInterfaces;
+using MediatR;
+
+namespace Library2._2.Commands.UserCommands
+{
+    public class AddUserCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public class AddUserCommandHandler : IRequestHandler<AddUserCommand, int>
+        {
+            private readonly IAddDeleteUser _addUser;
+
+            public AddUserCommandHandler(IAddDeleteUser addUser)
+            {
+                _addUser = addUser;
+            }
+
+            public async Task<int> Handle(AddUserCommand command, CancellationToken cancellationToken)
+            {
+                return _addUser.Add(command.Name, command.Password);
+            }
+        }
+    }
+}
---
+++ Library2.2/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Library2._2.Commands.UserCommands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library2._2.Controllers
+{
+    [ApiController]
+    [Route("/api/User")]
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult<int>> AddUser([FromBody] AddUserCommand command, CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(command, cancellationToken);
+        }
+    }
+}

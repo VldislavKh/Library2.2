5c609604d049254e46ca9c298ddba33f5ff8bc8b
VldislavKh <vkh123w@gmail.com>
2023-07-21 16:58:45 +0300

    добавлен интерфейс IAddDeleteRole и RoleService  с его реализацией

---
+++ Library2.2/Interfaces/RoleInterfaces/IAddDeleteRole.cs
@@ -0,0 +1,9 @@
+﻿namespace Library2._2.Interfaces.RoleInterfaces
+{
+    public interface IAddDeleteRole
+    {
+        public int Add(string name);
+
+        public void Delete(int Id);
+    }
+}
---
+++ Library2.2/Services/RoleService.cs
@@ -0,0 +1,37 @@
+﻿using Library2._2.Entities;
+using Library2._2.Infrastructure;
+using Library2._2.Interfaces.RoleInterfaces;
+
+namespace Library2._2.Services
+{
+    public class RoleService : IAddDeleteRole
+    {
+        private readonly ApplicationContext _context;
+
+        public RoleService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public int Add(string name)
+        {
+            var role = new Role();
+            role.Name = name;
+
+            _context.Add(role);
+            _context.SaveChanges();
+
+            return role.Id;
+        }
+
+        public void Delete(int id)
+        {
+            var role = _context.Roles.SingleOrDefault(x => x.Id == id);
+            if (role != null)
+            {
+                _context.Roles.Remove(role);
+                _context.SaveChanges();
+            }
+        }
+    }
+}

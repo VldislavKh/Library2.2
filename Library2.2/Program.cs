48ddaee9adf193e4b8dc1c1fdfbfb47e97f50f25
VldislavKh <vkh123w@gmail.com>
2023-07-21 12:43:06 +0300

    регистрация AuthorService и IAddDeleteAuthor в DI

--- Library2.2/Program.cs
+++ Library2.2/Program.cs
@@ -1,4 +1,6 @@
 using Library2._2.Infrastructure;
+using Library2._2.Interfaces.AuthorInterfaces;
+using Library2._2.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Library2._2
@@ -14,6 +16,9 @@ namespace Library2._2
             builder.Services.AddControllers();
             builder.Services.AddDbContext<ApplicationContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            builder.Services.AddScoped<IAddDeleteAuthor, AuthorService>();
 
             var app = builder.Build();
 

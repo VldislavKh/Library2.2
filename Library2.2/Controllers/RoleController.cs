1975abf163fab81031fd1f56b6b4fa26ff86b4b9
VldislavKh <vkh123w@gmail.com>
2023-07-21 17:00:38 +0300

    добавлен RoleController

---
+++ Library2.2/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library2._2.Controllers
+{
+    [ApiController]
+    [Route("/api/Role")]
+    public class RoleController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public RoleController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+    }
+}

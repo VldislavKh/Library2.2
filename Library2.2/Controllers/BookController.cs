f8ca8c82811005fee1272e643b937507cd2b9f78
VldislavKh <vkh123w@gmail.com>
2023-07-21 13:51:58 +0300

    добавлен запрос GetAuthorByIdQuery и его вызов в контроллере

--- Library2.2/Controllers/BookController.cs
+++ Library2.2/Controllers/BookController.cs
@@ -1,6 +1,42 @@
-﻿namespace Library2._2.Controllers
+﻿using Library2._2.Commands.BookCommands;
+using Library2._2.Queries.BookQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library2._2.Controllers
 {
-    public class BookController
+    [ApiController]
+    [Route("/api/Book")]
+    public class BookController : Controller
     {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPut("[action]")]
+        //Добавляет книгу в БД
+        public async Task<ActionResult<int>> AddBook([FromBody] AddBookCommand command, CancellationToken token)
+        {
+            return await _mediator.Send(command, token);
+        }
+
+        [HttpDelete("[action]/{id}")]
+        //Удаляет книгу из БД
+        public async Task<IActionResult> DeleteBook(int id, CancellationToken token)
+        {
+            await _mediator.Send(new DeleteBookCommand() { Id = id }, token);
+            return NoContent();
+        }
+
+        [HttpGet("[action]/{id}")]
+        //Возвращает автора книги по ее названию
+        public async Task<IActionResult> GetAuthor(int id, CancellationToken token)
+        {
+            var author = await _mediator.Send(new GetAuthorByIdQuery() { Id = id }, token);
+            return CreatedAtAction("GetAuthorByTitle", author);
+        }
     }
 }
---
+++ Library2.2/Queries/BookQueries/GetAuthorByIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using Library2._2.Entities;
+using Library2._2.Interfaces.BookInterfaces;
+using MediatR;
+
+namespace Library2._2.Queries.BookQueries
+{
+    public class GetAuthorByIdQuery : IRequest<Author>
+    {
+        public int Id { get; set; }
+        public class GetAuthorByIdQueryHandler : IRequestHandler<GetAuthorByIdQuery, Author>
+        {
+            private readonly IGetBooksInfo _getBooksInfo;
+
+            public GetAuthorByIdQueryHandler(IGetBooksInfo getAuthorsInfo)
+            {
+                _getBooksInfo = getAuthorsInfo;
+            }
+
+            public async Task<Author> Handle(GetAuthorByIdQuery query, CancellationToken cancellationToken)
+            {
+                return _getBooksInfo.GetAuthor(query.Id);
+            }
+        }
+
+    }
+}

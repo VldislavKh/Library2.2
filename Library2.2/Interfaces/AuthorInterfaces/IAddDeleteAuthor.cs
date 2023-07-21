299bb92b824b2f3bdd86addd5efca5797abf8352
VldislavKh <vkh123w@gmail.com>
2023-07-21 11:29:55 +0300

    Добавлен интерфейс IAddDeleteAurhor

---
+++ Library2.2/Interfaces/AuthorInterfaces/IAddDeleteAuthor.cs
@@ -0,0 +1,8 @@
+﻿namespace Library2._2.Interfaces.AuthorInterfaces
+{
+    public interface IAddDeleteAuthor
+    {
+        public int Add(string name, DateOnly birthDate, DateOnly? deathDate);
+        public void Delete(int id);
+    }
+}
